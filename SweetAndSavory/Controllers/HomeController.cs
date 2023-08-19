90c5881a63e5b8a61205f95d45b591b242e000c4
ShanaySharif <Shanay.uw17@gmail.com>
2023-08-19 15:09:28 -0400

    extra Sweet directory

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -19,47 +19,15 @@ namespace SweetAndSavory.Controllers
             _db = db;
         }
 
-        // [HttpGet("/")]
-        // public async Task<ActionResult> Index()
-        // {
-        //     if (User.Identity.IsAuthenticated){
-        //     Flavor[] flavors = _db.Flavors.ToArray();
-        //     Dictionary<string, object[]> model = new Dictionary<string, object[]>();
-        //     model.Add("flavors", flavors);
-        //     string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        //     ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-        //     if (currentUser != null)
-        //     {
-        //         Treat[] treats = _db.Treats
-        //             // .Where(entry => entry.User.Id == currtUser.Id)
-        //             .ToArray();
-        //         model.Add("treats", treats);
-        //     }
-        //     return View(model);
-        //     }
-        //     else{
-        //         return View(_db.Flavors.ToList());
-        
-
     [HttpGet("/")]
 public async Task<IActionResult> Index()
         {
-
-            //  if (User.Identity.IsAuthenticated){
-
             Treat[] treats = _db.Treats.ToArray();
             Flavor[] flavors = _db.Flavors.ToArray();
             Dictionary<string, object[]> model = new Dictionary<string, object[]>();
             model.Add("treats", treats);
             model.Add("flavors", flavors);
-
-
-        //     string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-        // ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-        //  if (currentUser != null)
-        //  {
-
-            return View(model);
+            return View(model);   
         }
     }
 }
